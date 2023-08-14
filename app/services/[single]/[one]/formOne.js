5eb867f6b9dd8acaebc3ea654d4513e738c0292a
smruti12346 <smruti.thetechnovate@gmail.com>
2023-08-14 17:49:19 +0530

    14th aug commit

---
+++ app/services/[single]/[one]/formOne.js
@@ -0,0 +1,182 @@
+"use client";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+async function handleSubmit(values, { resetForm }) {
+  try {
+    const response = await axios.post(
+      "https://huak-api.thecbdworld.org/wp-json/contact-form-7/v1/contact-forms/749/feedback",
+      values,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+
+    if (response.status === 200) {
+      console.log("Form data sent successfully!");
+    } else {
+      console.error("Failed to send form data.");
+    }
+    resetForm();
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+}
+
+const validationSchema = Yup.object().shape({
+  // first: Yup.string().trim().required('*Name is required').min(3, '*Name must be at least 3 characters').max(50, '*Name can be at most 50 characters'),
+  // email: Yup.string().trim().required('*Email is required').email('*Invalid email format').max(100, '*Email can be at most 100 characters'),
+  // phone: Yup.string().trim().required('*Mobile number is required').matches(/^[0-9]+$/, '*Mobile number must contain only digits').min(10, '*Mobile number must be at least 10 digits').max(15, '*Mobile number can be at most 15 digits'),
+});
+const FormOne = ({ name }) => {
+  const [loader, setLoader] = useState(false);
+  const [message, setMessage] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+  const handleInput = (event) => {
+    setForm((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const handleSubmit = () => {
+    setLoader(true);
+    setMessage("");
+    let formData = new FormData();
+    formData.append("first", form.name);
+    formData.append("email", form.email);
+    formData.append("phone", form.phone);
+    formData.append("service", name);
+    formData.append("message", form.message);
+    console.log(formData);
+
+    axios
+      .post(
+        "https://huak-api.thecbdworld.org/wp-json/contact-form-7/v1/contact-forms/749/feedback",
+        formData
+      )
+      .then((data) => {
+        setLoader(false);
+        data.data.status === "mail_sent" ? setSuccess(true) : setSuccess(false);
+        data.data.status === "mail_sent"
+          ? setForm({
+              name: "",
+              email: "",
+              phone: "",
+              message: "",
+            })
+          : "";
+        setMessage(data.data.message);
+      });
+  };
+  return (
+    <>
+      <div className="col-12 col-sm-12">
+        <section className="contact_form_area contact_us">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <div className="contact-right contact_details">
+                  <div className="hero-title-with-shape">
+                    <Link href="#scr">
+                      <h4 className="btn-yellow">Contact with us</h4>
+                    </Link>
+                    <h1>
+                      We offer 24/7 emergency service to all of our customers
+                    </h1>
+                  </div>
+                  {/*end .hero-title-with-shape*/}
+                  <p>
+                    There are many variations of passages of new lorem ipsum
+                    available, but the majority have suffered.
+                  </p>
+                </div>
+              </div>
+              <div
+                className="col-md-12"
+                id="scr"
+              >
+                <div className="contact-right contact-right-style-2 responsive_mt">
+                  <form
+                    action="#"
+                    method="post"
+                  >
+                    <input
+                      className="half_width input_m_right"
+                      type="text"
+                      name="name"
+                      placeholder="Your name"
+                      onChange={handleInput}
+                      value={form.name}
+                    />
+                    <input
+                      className="half_width"
+                      type="email"
+                      name="email"
+                      placeholder="Email address"
+                      onChange={handleInput}
+                      value={form.email}
+                    />
+                    <input
+                      type="tel"
+                      name="phone"
+                      placeholder="Phone number"
+                      onChange={handleInput}
+                      value={form.phone}
+                    />
+                    <textarea
+                      name="message"
+                      id="content"
+                      cols={20}
+                      rows={5}
+                      placeholder="Write message"
+                      defaultValue={""}
+                      onChange={handleInput}
+                      value={form.message}
+                    />
+                    <h4 className={success ? "text-success" : "text-danger"}>
+                      {message}
+                    </h4>
+                    <button
+                      className="btn-yellow"
+                      value="SUBMIT NOW"
+                      onClick={handleSubmit}
+                      disabled={loader}
+                    >
+                      {loader ? (
+                        <Image
+                          src="/assets/images/loader6.gif"
+                          width={30}
+                          height={30}
+                          alt="loader"
+                          style={{ marginRight: "10px" }}
+                        />
+                      ) : (
+                        ""
+                      )}
+                      SUBMIT NOW
+                    </button>
+                  </form>
+                  <div style={{ marginTop: "20px" }}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default FormOne;
--- app/services/[single]/[one]/page.js
+++ app/services/[single]/[one]/page.js
@@ -1,17 +1,9 @@
-"use client"
-import React from 'react';
-import Link from 'next/link';
-import axios from 'axios';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import { api_url } from '@/Auth';
-
-const validationSchema = Yup.object().shape({
-  // first: Yup.string().trim().required('*Name is required').min(3, '*Name must be at least 3 characters').max(50, '*Name can be at most 50 characters'),
-  // email: Yup.string().trim().required('*Email is required').email('*Invalid email format').max(100, '*Email can be at most 100 characters'),
-  // phone: Yup.string().trim().required('*Mobile number is required').matches(/^[0-9]+$/, '*Mobile number must contain only digits').min(10, '*Mobile number must be at least 10 digits').max(15, '*Mobile number can be at most 15 digits'),
-});
-
+import React from "react";
+import Link from "next/link";
+import { api_url } from "@/Auth";
+import FormOne from "./formOne";
+import Image from "next/image";
+import { url } from "@/Auth";
 const getData = async (slug) => {
   const res = await fetch(`${api_url}/pages?slug=${slug}`, {
     next: { revalidate: 2 },
@@ -20,49 +12,19 @@ const getData = async (slug) => {
   return data;
 };
 
-async function handleSubmit(values, { resetForm }) {
-  try {
-    const response = await axios.post(
-      'https://huak-api.thecbdworld.org/wp-json/contact-form-7/v1/contact-forms/749/feedback',
-      values,
-      {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      }
-    );
-
-    if (response.status === 200) {
-      console.log('Form data sent successfully!');
-    } else {
-      console.error('Failed to send form data.');
-    }
-    resetForm();
-  } catch (error) {
-    console.error('An error occurred:', error);
-  }
-}
-
 export async function generateMetadata({ params }) {
   const product = await getData(params.one);
   return {
-    title: product?.[0]?.acf?.meta_title || 'Default Title',
+    title: product?.[0]?.acf?.meta_title || "Default Title",
     openGraph: {
-      title: product?.[0]?.acf?.meta_title || 'Default Title',
-      description: product?.[0]?.acf?.meta_description || '',
+      title: product?.[0]?.acf?.meta_title || "Default Title",
+      description: product?.[0]?.acf?.meta_description || "",
     },
   };
 }
 
 async function Page(props) {
   let data = await getData(props.params.one);
-  const initialValues = {
-    first: '',
-    email: '',
-    phone: '',
-    message: '',
-    id: '749',
-  };
 
   return (
     <>
@@ -76,83 +38,97 @@ async function Page(props) {
         <h1>Our services</h1>
       </section>
       <div className="col-12 col-sm-12">
-      <div className="service_style_3_area text-center section_padding one-section">
-        <div className="container">
-          <div className="row">
-            <div className="col-12 col-sm-12">
-              <div
-                style={{
-                  width: "30px",
-                  height: "4px",
-                  marginLeft: "3px",
-                  marginBottom: "5px",
-                  backgroundColor: "#D8B14B",
-                }}
-              ></div>
-              <h3
-                style={{
-                  textAlign: "left",
-                  color: "#000",
-                  marginBottom: "15px",
-                }}
-              >
-                {data[0].title?.rendered}
-              </h3>
-            </div>
-        
-            
-              <div
-                style={{ textAlign: "left", fontWeight: "400" }}
-                dangerouslySetInnerHTML={{ __html: data[0].content.rendered }}
-              ></div>
-               <section>
-          <div className="col-12 col-sm-12">
-      <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
-        <div className="contact-right contact-right-style-2 responsive_mt">
-       
-        <Form>
-          {/* <ErrorMessage name="first" component="div" style={{ color: 'red' }} className="error-message" /> */}
-          <input type="text" name="first" placeholder="Your name" className="half_width input_m_right" style={{background:"#ddd"}} />
-          {/* <ErrorMessage name="email" component="div" style={{color:"red"}} className="error-message" style={{background:"#dbd5d5"}} /> */}
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Email address"
-                  className="half_width "  style={{background:"#ddd"}} 
-            />
-               
-               {/* <ErrorMessage name="phone" component="div" style={{color:"red"}} className="error-message" /> */}
-                <input
-                type="tel"
-                name="phone"
-                placeholder="Phone number"    style={{background:"#ddd"}}            
-               />
-               <input type="hidden"  name="service" />
-              
-                <textarea
-                    name="message"
-                    id="content"
-                    cols={160}
-                    rows={10}
-                    placeholder="Write message"
-                    defaultValue={""} style={{background:"#ddd"}}
-              >
-                
-              </textarea>
-           
+        <div className="service_style_3_area text-center section_padding one-section">
+          <div className="container">
+            <div className="row">
+              <div className="col-12 col-sm-12">
+                <div
+                  style={{
+                    width: "30px",
+                    height: "4px",
+                    marginLeft: "3px",
+                    marginBottom: "5px",
+                    backgroundColor: "#D8B14B",
+                  }}
+                ></div>
+                <h3
+                  style={{
+                    textAlign: "left",
+                    color: "#000",
+                    marginBottom: "15px",
+                  }}
+                >
+                  {data[0].title?.rendered}
+                </h3>
+              </div>
 
-          <button type="submit" className="btn-yellow">
-            Submit Now
-          </button>
-        </Form>
-       
+              <div class="row">
+                <div class="col-md-4">
+                  <Image
+                    class="alignnone size-medium wp-image-251"
+                    src={`${url}/${data?.[0]?.acf?.certificate_url}`}
+                    alt=""
+                    width="600"
+                    height="600"
+                    style={{ width: "100%", height: "auto" }}
+                  />
+                  <section className="contact_form_area contact_us section_padding">
+                    <div className="container">
+                      <div className="row">
+                        <div className="col-md-12">
+                          <div
+                            className="contact-right contact_details"
+                            style={{
+                              paddingRight: "0px",
+                              border: "1px solid #eceaea",
+                              boxShadow: "1px 1px 2px -2px",
+                            }}
+                          >
+                            <div>
+                              <div
+                                className="contact_location_map col-12"
+                                style={{
+                                  width: "100%",
+                                }}
+                              >
+                                <span className="icon-location" />
+                                <p>
+                                  Address：1-2F, B2 Building, Junfeng Zhongcheng
+                                  Zhizao Innovation Park,
+                                  <br /> Heping Community, Fuhai Street,Bao'an
+                                  District, Shenzhen, China{" "}
+                                </p>
+                              </div>
+                              {/*end .contact_location_map*/}
+                              <div className="contact_location_map contact_location_call col-12">
+                                <span className="icon-contact_call" />
+                                <p>
+                                  patty@cer-mark.com
+                                  <br />
+                                  Phone: +86-135-28437881 <br />
+                                  Whatsapp: +86-135-28437881
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </section>
+                </div>
+                <div class="col-md-8">
+                  <div
+                    style={{ textAlign: "left", fontWeight: "400" }}
+                    dangerouslySetInnerHTML={{
+                      __html: data[0].content.rendered,
+                    }}
+                  ></div>
+                  <FormOne name={props.params.one} />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </Formik>
-      </div>
-      </section>
-      </div>
-      </div>
-      </div>
       </div>
     </>
   );
