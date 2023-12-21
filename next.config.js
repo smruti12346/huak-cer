ab2561af4643ec506576866630faa4e0b122c79f
smruti12346 <smruti.thetechnovate@gmail.com>
2023-12-21 17:59:04 +0530

    21st second commit

--- next.config.js
+++ next.config.js
@@ -14,6 +14,47 @@ const nextConfig = {
         destination: "/",
         permanent: true,
       },
+      {
+        source: "/services/eu-certification-project/ce-certification",
+        destination: "/ce-certification-europe/",
+        permanent: true,
+      },
+      {
+        source: "/services/american-certification/fcc-certification-of-us",
+        destination: "/fcc-certification/",
+        permanent: true,
+      },
+      {
+        source: "/services/american-certification/ic-certification-of-canada",
+        destination: "/ic-certification/",
+        permanent: true,
+      },
+      {
+        source:
+          "/services/certification-project-of-asian/telec-certification-of-japan",
+        destination: "/TELEC-certification/",
+        permanent: true,
+      },
+      {
+        source: "/services/eu-certification-project/rohs-testing",
+        destination: "/rohs-certification/",
+        permanent: true,
+      },
+      {
+        source: "/services/eu-certification-project/reach-certification",
+        destination: "/reach-certification/",
+        permanent: true,
+      },
+      {
+        source: "/services/eu-certification-project/reach-certification",
+        destination: "/reach-compliance-certificate/",
+        permanent: true,
+      },
+      {
+        source: "/services/american-certification/cpc-certification",
+        destination: "/american-children-product-certification/",
+        permanent: true,
+      },
     ];
   },
 };
